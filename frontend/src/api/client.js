4a2eee36ec72d80afba413461b32efc1891d159f
Rifat Hassan <rifathassan11.29@gmail.com>
2026-05-15 00:20:31 +0600

    Fixed API URLs for production

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
-    ? `${import.meta.env.VITE_API_URL}/api`
-    : "/api",
+  baseURL: "https://edutrack-z7gs.onrender.com/api",
 });
 
 client.interceptors.request.use((config) => {
